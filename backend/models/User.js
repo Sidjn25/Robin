87a82533e8315537e86fe61c19baec8f8ade65fd
SID-HCDS <siddharth_jain@dudescript.com>
2025-08-20 01:24:01 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String // Store hashed password in real app!
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true } // Store hashed password in real app!
 });
 module.exports = mongoose.model('User', userSchema);
